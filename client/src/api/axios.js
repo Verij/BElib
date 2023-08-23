2ccf0d26597b30a37be27a962c28ad35dff3e878
Verij <95141807+Verij@users.noreply.github.com>
2023-08-23 19:43:08 -0300

    Update axios.js

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -3,7 +3,8 @@ import axios from 'axios';
 
 
 const instance = axios.create({
-  baseURL:'https://belib.onrender.com/api'
+  baseURL:'https://belib.onrender.com/api',
+  withCredentials: true
 });
 
 export default instance;
