fc7b12e02f41b7058bfaedbe7a81a578c301b00e
Verij <95141807+Verij@users.noreply.github.com>
2023-08-23 12:17:01 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -10,7 +10,8 @@ import librosRoutes from './routes/libros-routes.js';
 const app = express();
 
 app.use(cors({
-  origin:'http://localhost:5173',
+  origin:'https://powerful-pike-shirt.cyclic.app
+',
   credentials: true
 }));
 app.use(morgan("dev"));
