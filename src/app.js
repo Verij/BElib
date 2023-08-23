429575e8fcd2bf2bd59998ae3897c28db8aa369a
Verij <95141807+Verij@users.noreply.github.com>
2023-08-23 14:57:17 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -10,10 +10,10 @@ import librosRoutes from './routes/libros-routes.js';
 const app = express();
 
 app.use((req, res, next) => {
-      res.header("Access-Control-Allow-Origin", "`https://b-elib-toea.vercel.app/`");
-      res.header("Access-Control-Allow-Credentials", true);
+      res.header("Access-Control-Allow-Origin", "https://b-elib-toea.vercel.app/");
+      res.header("Access-Control-Allow-Credentials: true");
       res.header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
-      res.header("Access-Control-Allow-Headers: Content-Type", *);
+      res.header("Access-Control-Allow-Headers: Content-Type, *");
       next();
 });
 
