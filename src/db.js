928ec1e14dd193459216964fef036f07ac6df9c5
Verij <95141807+Verij@users.noreply.github.com>
2023-08-23 20:59:47 -0300

    Update db.js

--- src/db.js
+++ src/db.js
@@ -1,8 +1,10 @@
 import mongoose from 'mongoose';
+import { MONGODB_URI } from './config.js';
+
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb+srv://admin:jyUm9sF9qY61VEKG@cluster0.vrlfm6b.mongodb.net/?retryWrites=true&w=majority');
+    await mongoose.connect(MONGODB_URI);
     console.log("esta conectada la DB")
   } catch (error) {
     console.log(error)
