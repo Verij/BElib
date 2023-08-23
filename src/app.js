7d83ca6b562f2f92c90d96cf2f1791504d0ac596
Verij <95141807+Verij@users.noreply.github.com>
2023-08-23 13:31:33 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -9,6 +9,10 @@ import librosRoutes from './routes/libros-routes.js';
 
 const app = express();
 
+app.use(cors({
+  origin:'http://localhost:5173',
+  credentials: true
+}));
 
 app.use(morgan("dev"));
 app.use(express.json());
