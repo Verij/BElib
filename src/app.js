a762ab9d32f0bf078c4f169e3a3e8e667569e731
Verij <95141807+Verij@users.noreply.github.com>
2023-08-23 19:22:21 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ import librosRoutes from './routes/libros-routes.js';
 const app = express();
 
 app.use(cors({
-  origin:'https://b-elib-toea.vercel.app/',
+  origin:'http://localhost:5173',
   credentials: true
 }));
 
